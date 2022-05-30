670304e0d73d304043a106148f1fc0a58eeb7ca8
Jens Morten Mikkelsen <jmm@vertica.dk>
2022-05-30 10:19:00 +0200

    updated storybook main.js to follow repo by shilman

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,24 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "features": {
-    "storyStoreV7": true
-  }
+    stories: [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions"
+    ],
+    framework: "@storybook/vue",
+    core: {
+        builder: "storybook-builder-vite-vue2"
+    },
+    viteFinal: (config) => {
+        // customize the Vite config here
+
+        // https://github.com/storybookjs/storybook/issues/10887#issuecomment-901109891
+        // config.resolve.dedupe = ['@storybook/client-api'];
+        console.log({ config });
+
+        return config;
+    },
 }
